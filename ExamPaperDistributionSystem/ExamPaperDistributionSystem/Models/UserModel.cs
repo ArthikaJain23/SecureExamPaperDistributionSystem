8b41f075a398420526f97c132ce4b41ad5ff57cf
ArthikaJain23 <123855149+ArthikaJain23@users.noreply.github.com>
2024-06-29 16:58:07 +0530

    Initial Commit

---
+++ ExamPaperDistributionSystem/ExamPaperDistributionSystem/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamPaperDistributionSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
